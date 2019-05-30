f6aa222d5150fdfb28b583e87e142d6384dc1650
Daniele Schiavi <schiavi91@gmail.com>
2019-05-30 18:05:01 +0200

    Setup login fragment

--- app/src/main/java/it/unibo/preh_frontend/home/BottomSheetFragmentExample.kt
+++ app/src/main/java/it/unibo/preh_frontend/home/BottomSheetFragmentExample.kt
@@ -9,6 +9,6 @@ import it.unibo.preh_frontend.R
 
 class BottomSheetFragmentExample : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 }
--- app/src/main/java/it/unibo/preh_frontend/home/HomeFragment.kt
+++ app/src/main/java/it/unibo/preh_frontend/home/HomeFragment.kt
@@ -20,7 +20,7 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_login, container, false)
 
         /* val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
