c07450aa67be72e4ba35bfcdf1c5c5c855f8698d
Nicola Atti <nicola.atti@studio.unibo.it>
2019-06-04 19:11:51 +0200

    Layout adjustments

--- app/src/main/java/it/unibo/preh_frontend/dialog/NewPcCarBottomSheetFragment.kt
+++ app/src/main/java/it/unibo/preh_frontend/dialog/NewPcCarBottomSheetFragment.kt
@@ -4,11 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import it.unibo.preh_frontend.R
 
+
+
+
 class NewPcCarBottomSheetFragment : BottomSheetDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_pccar, container, false)
+        val root = inflater.inflate(R.layout.fragment_pccar, container, false)
+
+        /*dialog!!.setOnShowListener { dialog ->
+            val d = dialog as BottomSheetDialog
+            val bottomSheet = d.findViewById<View>(R.id.pc_car) as ConstraintLayout
+            val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            bottomSheetBehavior.peekHeight = 200
+        }*/
+
+        return root
     }
 }
