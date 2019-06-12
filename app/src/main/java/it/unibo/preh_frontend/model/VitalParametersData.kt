3436caf686e43f98fc5b1ef8c11739de6b8206a0
Nicola Atti <nicola.atti@studio.unibo.it>
2019-06-12 17:26:24 +0200

    Add patient model data

---
+++ app/src/main/java/it/unibo/preh_frontend/model/VitalParametersData.kt
@@ -0,0 +1,19 @@
+package it.unibo.preh_frontend.model
+
+class VitalParametersData(var vieAeree: Int,
+                          var frequenzaRespiratoria: Int,
+                          var saturazionePeriferica: Int,
+                          var frequenzaCaridaca: Int,
+                          var tipoBattito: Int,
+                          var pressioneArteriosa: Int,
+                          var tempoRiempimentoCapillare: Int,
+                          var coloritoCuteMucose: Int,
+                          var aperturaOcchi: Int,
+                          var rispostaVerbale: Int,
+                          var rispostaMotore: Int,
+                          var pupilleSx: Int,
+                          var pupilleDx: Int,
+                          var fotoreagenteSx: Boolean,
+                          var fotoreagenteDx: Boolean,
+                          var temperature: Int) {
+}
