a991734e2a9bad7e8696d05556a3e0f842051e90
Daniele Schiavi <schiavi91@gmail.com>
2019-06-13 18:47:53 +0200

    Add methods to change style of selectable button from code

--- app/src/main/java/it/unibo/preh_frontend/utils/ButtonAppearance.kt
+++ app/src/main/java/it/unibo/preh_frontend/utils/ButtonAppearance.kt
@@ -1,4 +1,28 @@
 package it.unibo.preh_frontend.utils
 
+import android.content.res.Resources
+import android.os.Build
+import android.widget.Button
+import it.unibo.preh_frontend.R
+
 object ButtonAppearance {
+    fun activateButton(button: Button, resources: Resources) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            button.backgroundTintList = resources.getColorStateList(R.color.buttonSelectedBackgroundColor, null)
+            button.setTextColor(resources.getColorStateList(R.color.buttonUnselectedBackgroundColor, null))
+        } else {
+            button.backgroundTintList = resources.getColorStateList(R.color.buttonSelectedBackgroundColor)
+            button.setTextColor(resources.getColorStateList(R.color.buttonUnselectedBackgroundColor))
+        }
+    }
+
+    fun deactivateButton(button: Button, resources: Resources) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            button.backgroundTintList = resources.getColorStateList(R.color.buttonUnselectedBackgroundColor, null)
+            button.setTextColor(resources.getColorStateList(R.color.buttonSelectedBackgroundColor, null))
+        } else {
+            button.backgroundTintList = resources.getColorStateList(R.color.buttonUnselectedBackgroundColor)
+            button.setTextColor(resources.getColorStateList(R.color.buttonSelectedBackgroundColor))
+        }
+    }
 }
