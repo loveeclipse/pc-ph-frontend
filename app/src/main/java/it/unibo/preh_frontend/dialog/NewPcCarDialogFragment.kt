133ca45bf22a32ac2ff83319144cd2ed4ddf91f5
Chiara <chiara.volonnino@studio.unibo.it>
2019-06-13 15:50:16 +0200

    Add in all buttons in NewPcCar fragment dialog NewPcCarItems.

--- app/src/main/java/it/unibo/preh_frontend/dialog/NewPcCarDialogFragment.kt
+++ app/src/main/java/it/unibo/preh_frontend/dialog/NewPcCarDialogFragment.kt
@@ -8,9 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
-import it.unibo.preh_frontend.model.*
 import androidx.fragment.app.DialogFragment
-import androidx.navigation.fragment.findNavController
 import it.unibo.preh_frontend.R
 
 
@@ -21,8 +19,23 @@ class NewPcCarDialogFragment : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_pccar, container, false)
 
-        val insertPlace =  root.findViewById<Button>(R.id.crew_departure_button)
-        insertPlace.setOnClickListener {
+        val insertItemsCrewDeparture =  root.findViewById<Button>(R.id.crew_departure_button)
+        insertItemsCrewDeparture.setOnClickListener {
+            NewPcCarItemsDialogFragment().show(requireActivity().supportFragmentManager, "layout/fragment_pccar_items_dialog.xml")
+        }
+
+        val insertItemsArrivalOnSite =  root.findViewById<Button>(R.id.arrival_on_site_button)
+        insertItemsArrivalOnSite.setOnClickListener {
+            NewPcCarItemsDialogFragment().show(requireActivity().supportFragmentManager, "layout/fragment_pccar_items_dialog.xml")
+        }
+
+        val insertItemsDepartureFromSite =  root.findViewById<Button>(R.id.departure_from_site_button)
+        insertItemsDepartureFromSite.setOnClickListener {
+            NewPcCarItemsDialogFragment().show(requireActivity().supportFragmentManager, "layout/fragment_pccar_items_dialog.xml")
+        }
+
+        val insertItemsLandingHelipad =  root.findViewById<Button>(R.id.landing_helipad_button)
+        insertItemsLandingHelipad.setOnClickListener {
             NewPcCarItemsDialogFragment().show(requireActivity().supportFragmentManager, "layout/fragment_pccar_items_dialog.xml")
         }
 
