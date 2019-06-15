9c916dc547df76a131cd0570fe3fd0d3e8a010ae
Daniele Schiavi <schiavi91@gmail.com>
2019-06-15 19:04:50 +0200

    Fix location error

--- app/src/main/java/it/unibo/preh_frontend/dialog/NewPcCarItemsDialogFragment.kt
+++ app/src/main/java/it/unibo/preh_frontend/dialog/NewPcCarItemsDialogFragment.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.app.Dialog
 import android.content.DialogInterface
 import android.location.Geocoder
-import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,15 +11,23 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.fragment.app.DialogFragment
-import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import it.unibo.preh_frontend.R
 import it.unibo.preh_frontend.utils.PermissionManager
+import java.io.IOException
 import java.util.Locale
 
 class NewPcCarItemsDialogFragment : DialogFragment() {
-    private var fusedLocationClient: FusedLocationProviderClient? = null
     private lateinit var placeEditText: EditText
+    private val reqSetting = LocationRequest.create().apply {
+        fastestInterval = 1000
+        interval = 1000
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        smallestDisplacement = 1.0f
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_pccar_items_dialog, container, false)
@@ -28,9 +35,7 @@ class NewPcCarItemsDialogFragment : DialogFragment() {
 
         placeEditText = root.findViewById(R.id.place_edit_text)
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-
-        updateLastAddress()
+        updateLocation()
 
         val saveAndExitButton = root.findViewById<ImageButton>(R.id.pccar_items_image_button)
         saveAndExitButton.setOnClickListener {
@@ -57,18 +62,28 @@ class NewPcCarItemsDialogFragment : DialogFragment() {
         }
     }
 
-    private fun updateLastAddress() {
-        if (PermissionManager.checkPermission(requireActivity(), requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)) {
-            fusedLocationClient?.lastLocation?.addOnSuccessListener(requireActivity()) {
-                location: Location ->
-                    location.apply {
+    private fun updateLocation() {
+        if (PermissionManager.checkPermission(requireActivity(), requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            val locationUpdates = object : LocationCallback() {
+                override fun onLocationResult(localResult: LocationResult) {
+                    val locationUpdates = this
+                    localResult.locations.last().apply {
                         val geocoder = Geocoder(requireContext(), Locale.getDefault())
-                        val addresses = geocoder.getFromLocation(latitude, longitude, 1)
-                        if (addresses.isNotEmpty()) {
-                            placeEditText.setText(addresses[0].getAddressLine(0))
-                        }
+                        try {
+                            val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+                            if (addresses.isNotEmpty()) {
+                                requireActivity().runOnUiThread {
+                                    placeEditText.setText(addresses[0].getAddressLine(0))
+                                }
+                                fusedLocationClient?.removeLocationUpdates(locationUpdates)
+                            }
+                        } catch (ex: IOException) {}
                     }
+                }
             }
+            fusedLocationClient?.requestLocationUpdates(reqSetting, locationUpdates, null)
         }
     }
 }
