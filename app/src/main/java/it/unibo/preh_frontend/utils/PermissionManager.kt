e37cc71c6833f2369766f1c676430f2008f62282
Daniele Schiavi <schiavi91@gmail.com>
2019-06-15 13:25:55 +0200

    Always ask permissions if denied when they are checked

--- app/src/main/java/it/unibo/preh_frontend/utils/PermissionManager.kt
+++ app/src/main/java/it/unibo/preh_frontend/utils/PermissionManager.kt
@@ -3,33 +3,18 @@ package it.unibo.preh_frontend.utils
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 object PermissionManager {
+    const val LOCATION_ID = 42
+
     fun checkPermission(activity: Activity, context: Context, vararg perm: String): Boolean {
         val havePermissions = perm.toList().all {
             ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
         }
         if (!havePermissions) {
-            if (perm.toList().any {
-                        ActivityCompat.shouldShowRequestPermissionRationale(activity, it)
-                    }) {
-                val dialog = AlertDialog.Builder(context)
-                        .setTitle("Permission")
-                        .setMessage("Permission needed!")
-                        .setPositiveButton("OK") {
-                            _, _ -> ActivityCompat.requestPermissions(activity, perm, 0)
-                        }
-                        .setNegativeButton("No") {
-                            _, _ ->
-                        }
-                        .create()
-                dialog.show()
-            } else {
-                ActivityCompat.requestPermissions(activity, perm, 0)
-            }
+            ActivityCompat.requestPermissions(activity, perm, LOCATION_ID)
             return false
         }
         return true
