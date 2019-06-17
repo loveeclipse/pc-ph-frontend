af9db51a90e4186df3b16ece82b5ec4409073ff2
Chiara <chiara.volonnino@studio.unibo.it>
2019-06-17 09:28:47 +0200

    Add default value in all drugs.

---
+++ app/src/main/java/it/unibo/preh_frontend/dialog/utils/DrugsValue.kt
@@ -0,0 +1,12 @@
+package it.unibo.preh_frontend.dialog.utils
+
+object DrugsValue {
+    const val crystalloidValue = 500
+    const val succinylcholineValue = 100
+    const val fentanilValue = 10
+    const val ketamineValue = 100
+
+    const val millilitersUnit = "ml"
+    const val milligramsUnit = "mg"
+    const val microgramsUnit = "ug"
+}
--- app/src/main/java/it/unibo/preh_frontend/home/DrugsFragment.kt
+++ app/src/main/java/it/unibo/preh_frontend/home/DrugsFragment.kt
@@ -9,10 +9,18 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import it.unibo.preh_frontend.R
 import it.unibo.preh_frontend.dialog.InputDialogFragment
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.crystalloidValue
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.succinylcholineValue
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.fentanilValue
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.ketamineValue
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.millilitersUnit
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.milligramsUnit
+import it.unibo.preh_frontend.dialog.utils.DrugsValue.microgramsUnit
 
 class DrugsFragment : Fragment() {
 
     private lateinit var sharedPreferences: SharedPreferences
+    private val inputDialogLayout = "layout/input_dialog.xml"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,27 +29,34 @@ class DrugsFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_drugs, container, false)
 
-        val dialog = InputDialogFragment()
         val insertCrystalloid = root.findViewById<Button>(R.id.crystalloid_button)
         insertCrystalloid.setOnClickListener {
-            dialog.show(requireActivity().supportFragmentManager, "layout/input_dialog.xml")
+            val dialog = InputDialogFragment()
+            dialog.show(requireActivity().supportFragmentManager, inputDialogLayout)
+            dialog.setInput(crystalloidValue, millilitersUnit)
         }
-        dialog.setInput(500, "ml")
 
         val insertSuccinylcholine = root.findViewById<Button>(R.id.succinylcholine_button)
         insertSuccinylcholine.setOnClickListener {
-            InputDialogFragment().show(requireActivity().supportFragmentManager, "layout/input_dialog.xml")
+            val dialog = InputDialogFragment()
+            dialog.show(requireActivity().supportFragmentManager, inputDialogLayout)
+            dialog.setInput(succinylcholineValue, milligramsUnit)
         }
 
         val insertFentanil = root.findViewById<Button>(R.id.fentanil_button)
         insertFentanil.setOnClickListener {
-            InputDialogFragment().show(requireActivity().supportFragmentManager, "layout/input_dialog.xml")
+            val dialog = InputDialogFragment()
+            dialog.show(requireActivity().supportFragmentManager, inputDialogLayout)
+            dialog.setInput(fentanilValue, microgramsUnit)
         }
 
         val insertKetamine = root.findViewById<Button>(R.id.ketamine_button)
         insertKetamine.setOnClickListener {
-            InputDialogFragment().show(requireActivity().supportFragmentManager, "layout/input_dialog.xml")
+            val dialog = InputDialogFragment()
+            dialog.show(requireActivity().supportFragmentManager, inputDialogLayout)
+            dialog.setInput(ketamineValue, milligramsUnit)
         }
+
         // Inflate the layout for this fragment
         return root
     }
