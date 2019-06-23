9f38dcc2d0dc7f0fb5cfbc99e13388157eb4a9e2
Daniele Schiavi <schiavi91@gmail.com>
2019-06-23 15:31:18 +0200

    Add implementation of history manager

--- app/src/main/java/it/unibo/preh_frontend/utils/HistoryManager.kt
+++ app/src/main/java/it/unibo/preh_frontend/utils/HistoryManager.kt
@@ -1,4 +1,41 @@
 package it.unibo.preh_frontend.utils
 
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import it.unibo.preh_frontend.model.AnagraphicData
+import it.unibo.preh_frontend.model.ComplicationsData
+import it.unibo.preh_frontend.model.DrugsData
+import it.unibo.preh_frontend.model.ManeuverData
+import it.unibo.preh_frontend.model.NewPcCarData
+import it.unibo.preh_frontend.model.PatientStatusData
+import it.unibo.preh_frontend.model.PreHData
+import it.unibo.preh_frontend.model.TreatmentData
+import it.unibo.preh_frontend.model.VitalParametersData
+
 object HistoryManager {
+    private val historyType = object : TypeToken<ArrayList<PreHData>>() {}.type
+    private val typeFactory = RuntimeTypeAdapterFactory
+            .of(PreHData::class.java, "type")
+            .registerSubtype(AnagraphicData::class.java, "AnagraphicData")
+            .registerSubtype(ComplicationsData::class.java, "ComplicationsData")
+            .registerSubtype(ManeuverData::class.java, "ManeuverData")
+            .registerSubtype(PatientStatusData::class.java, "PatientStatusData")
+            .registerSubtype(TreatmentData::class.java, "TreatmentData")
+            .registerSubtype(VitalParametersData::class.java, "VitalParametersData")
+            .registerSubtype(DrugsData::class.java, "DrugsData")
+            .registerSubtype(NewPcCarData::class.java, "NewPcCarData")
+
+    fun addVoice(data: PreHData, sharedPreferences: SharedPreferences) {
+        val localHistoryList = getHistoryList(sharedPreferences)
+        localHistoryList.add(data)
+        val historyListAsJson = Gson().toJson(localHistoryList, historyType)
+        sharedPreferences.edit().putString("historyList", historyListAsJson).apply()
+    }
+
+    fun getHistoryList(sharedPreferences: SharedPreferences): ArrayList<PreHData> {
+        val gson = GsonBuilder().setPrettyPrinting().registerTypeAdapterFactory(typeFactory).create()
+        return gson.fromJson(sharedPreferences.getString("historyList", null), historyType)
+    }
 }
