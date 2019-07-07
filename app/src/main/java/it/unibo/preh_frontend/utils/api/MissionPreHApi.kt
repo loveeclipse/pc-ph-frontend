399dd4e3cdd47a14a5936ef58e8a0191a042ea5f
Nicola Atti <nicola.atti@studio.unibo.it>
2019-07-07 18:43:46 +0200

    Initial addition of mission REST calls

--- app/src/main/java/it/unibo/preh_frontend/utils/api/MissionPreHApi.kt
+++ app/src/main/java/it/unibo/preh_frontend/utils/api/MissionPreHApi.kt
@@ -2,7 +2,7 @@ package it.unibo.preh_frontend.utils.api
 
 import it.unibo.preh_frontend.dt_model.TimelineItem
 import retrofit2.Call
-import retrofit2.http.Field
+import retrofit2.http.Body
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
@@ -10,15 +10,15 @@ interface MissionPreHApi {
     @PUT("/events-tracking/{eventId}/missions/{missionId}/chosen-hospital'")
     fun insertChosenHospital(@Path("eventId") eventId: String,
                              @Path("missionId") missionId: String,
-                             @Field("chosenHospital") chosenHospital: String) : Call<Int>
+                             @Body chosenHospital: String) : Call<Int>
 
     @PUT("/events-tracking/{eventId}/missions/{missionId}/timeline/oc-call'")
     fun setOcCall(@Path("eventId") eventId: String,
                   @Path("missionId") missionId: String,
-                  @Field("ocCall") ocCall: TimelineItem): Call<Int>
+                  @Body ocCall: TimelineItem): Call<Int>
 
     @PUT("/events-tracking/{eventId}/mission/{missionId}/timeline/crew-departure")
     fun setCrewDeparture(@Path("eventId") eventId: String,
                          @Path("missionId") missionId: String,
-                         @Field("crewDeparture") crewDeparture: TimelineItem): Call<Int>
+                         @Body crewDeparture: TimelineItem): Call<Int>
 }
